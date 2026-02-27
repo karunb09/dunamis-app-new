a30239a07e2bb9aa85ea537394d6c15741a533b0
Karun <karunb09@gmail.com>
2026-02-27 13:33:02 +0530

    added mail user in backend env and mailsender.js

--- dunamis-lms-backend-main/utils/mailSender.js
+++ dunamis-lms-backend-main/utils/mailSender.js
@@ -3,6 +3,8 @@ const mailSender = async (email, title, body, attachments = []) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
